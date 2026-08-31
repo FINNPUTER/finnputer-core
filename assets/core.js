3f2965502fba1a1aef9076287d98c0c216892720
FINNPUTER <partypeople3333@gmail.com>
2026-08-31 15:08:44 +0100

    Add files via upload

--- assets/core.js
+++ assets/core.js
@@ -30,7 +30,7 @@ function chrome_(active) {
   document.body.insertAdjacentHTML("afterbegin",
     '<div class="aurora"><i></i><i></i><i></i></div><div class="grain"></div>');
   const links = [
-    ["/", "Home"], ["/opportunities/", "Signals"], ["/runners/", "The record"],
+    ["/", "Home"], ["/opportunities/", "Signals"], ["/runners/", "Runners"],
     ["/buys/", "Live buys"],
     ["/smart-money/", "Smart money"],
     ["/token/", "Token"], ["/how/", "How it works"],
